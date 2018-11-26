73ca7a73925da3b13fec07c251f46bfe2e523cfb
Matthew Dodd <macbookpro@MacBook-Pro-de-MacBook.local>
2018-11-26 19:28:49 +0100

    added all functionality on script.js
    added functions to meet all desired funcs
    added to comments throughout

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Treehouse-Project-2
+
+This project displays a list of students.
+
+Using JS to paginate the long list of students. 10 per page.
+
+At the bottom of the page we can scroll through the pages of students.
+
+We can also search students using the search bar.
--- js/script.js
+++ js/script.js
@@ -2,49 +2,100 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+//Gather all students in array
+const studentsList = document.getElementsByClassName('student-item');
+//select div which conatins all content
+const div = document.querySelector('.page');
 
 
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+//function takes 1 param which is the page number to be shown and will diplay 10 students per page
+function showPage(pageNumber){
+  //declare 2 const, we will show the numbers between these 2 const
+  const higherThan = pageNumber * 10;
+  const lowerThan = higherThan - 10;
+  //loop through list of students
+  for(let i = 0; i < studentsList.length; i++){
+      if(i < lowerThan || i >= higherThan){
+        //set display to none for students outside these const values, this will hide them
+        studentsList[i].style.display = 'none';
+      } else{
+        //if inside the 2 const then ensure display is set to an empty string.
+        studentsList[i].style.display = '';
+      }
+  }
+}
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+//create a function to append links onto page.
+function appendPageLinks(){
+  //select div so that we can append to it
+  const div = document.querySelector('.page');
+  //create div which we will append into div on page
+  const newDiv = document.createElement('div');
+  //adding className for styling
+  newDiv.className = 'pagination';
+  //append newDiv into div on page
+  div.appendChild(newDiv);
+  //create ul which will append into div
+  const ul = document.createElement('ul');
+  //apend into newdiv
+  newDiv.appendChild(ul);
+  //calculate the amount of links we will need on page. takes length of students on page, divides by 10 then rounds to the nearest integer. e.g. 54 is length/10= 5.4 applying Math.ceil gives us 6 buttons.
+  const numOfLinks = Math.ceil((studentsList.length / 10));
+  //function that will create a link with param of pagenumber
+  function createLink(pageNumber){
+    //create li elem
+    const li = document.createElement('li');
+    //append it into ul which we created in appendPageLinks func
+    ul.appendChild(li);
+    //create our link element
+    const link = document.createElement('a');
+    //set textContent to the parameter passed into the func
+    link.textContent = pageNumber;
+    //we want the page to start with page 1 showing, so we will give it the active class and all links get the pageLink class
+    if(link.textContent === '1'){
+      link.className = 'pageLink active';
+    } else{
+      link.className = 'pageLink';
+    }
+    //finally append the link into the li where our links will be shown on the page
+    li.appendChild(link);
+  }
+  //loop through our const which contains the amount of links we need to show
+  for(let i = 1; i <= numOfLinks; i++){
+      //call the create link func as many times as we need. i is passed as argument which will be converted as link number inside func
+      createLink(i);
+  }
+}
+//declaring  function which we will call to set our page up when loaded
+function initPage(){
+  //creates our links
+  appendPageLinks();
+  //ensures that page 1 will be the default page when opened
+  showPage(1);
+}
+//Called to init page
+initPage();
 
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+//Event Listener for clicks on div
+div.addEventListener('click', (event) => {
+  //declare const for what was clicked
+  const linkClicked = event.target;
+  //Only run code if the click event was on a link we created
+  if(linkClicked.className === 'pageLink'){
+    //select links to loop through them
+    const links = document.getElementsByClassName('pageLink')
+    for(let i = 0; i < links.length; i++){
+      //remove the active class if needed
+        links[i].classList.remove('active');
+      }
+      //apply the active class to the link that was clicked
+    linkClicked.classList.add('active');
+    //grab textcontent of link that was clicked
+    const pageNumber = linkClicked.textContent;
+    //use parseint to convert pagenumber to integer, then pass as argument to showpage func
+    showPage(parseInt(pageNumber));
+  }
+});
