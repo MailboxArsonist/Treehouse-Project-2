42a962cc579ad705b88995f8f84f5465e1195c32
Matthew Dodd <macbookpro@MacBook-Pro-de-MacBook.local>
2018-11-28 10:45:22 +0100

    Class added to CSS file, major changes to JS file to accommodate new search function
    1.Added funcs to clear page, create message box, create search box.
    2.Added event listeners onto search box to create new list of students based on search results, then display new list.
    3.Edited original functions to better accommodate the new format.
    4.Small edit on CSS file to add a class for the message box created.

--- js/script.js
+++ js/script.js
@@ -2,60 +2,82 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-
-//Gather all students in array
-const studentsList = document.getElementsByClassName('student-item');
-//select div which conatins all content
+//Declaring consts
 const div = document.querySelector('.page');
+const divHeader = document.querySelector('.page-header');
+const studentsList = document.getElementsByClassName('student-item');
 
-
-
-
-//function takes 1 param which is the page number to be shown and will diplay 10 students per page
-function showPage(pageNumber){
-  //declare 2 const, we will show the numbers between these 2 const
-  const higherThan = pageNumber * 10;
-  const lowerThan = higherThan - 10;
-  //loop through list of students
+//define function so we can clear all students from page if needed
+const clearPage = () => {
   for(let i = 0; i < studentsList.length; i++){
-      if(i < lowerThan || i >= higherThan){
-        //set display to none for students outside these const values, this will hide them
-        studentsList[i].style.display = 'none';
-      } else{
-        //if inside the 2 const then ensure display is set to an empty string.
-        studentsList[i].style.display = '';
-      }
+    studentsList[i].style.display = 'none';
   }
 }
 
+//function takes 1 param which is the page number to be shown and will diplay 10 students per page
+const showPage = (pageNumber, stuList) => {
+  //declare 2 const, we will show the numbers between these 2 const
+  let higherThan = pageNumber * 10;
+  let lowerThan = higherThan - 10;
+  if(stuList.length < 10){
+    higherThan = stuList.length;
+    lowerThan = 0;
+  }
+  //clear page, then loop thorugh list and display students
+  clearPage();
+
+  for(let i = 0; i < stuList.length; i++){
+      if(i < lowerThan || i >= higherThan){
+        stuList[i].style.display = 'none';
+        } else{
+          stuList[i].style.display = '';
+        }
+  }
+}
+//func we will call to create a searchbox
+const createInputElement = () => {
+  //get div to append new element
+  const divHeader = document.querySelector('.page-header');
+  //create div, input and a button and append them inside the page-header div
+  const div = document.createElement('div');
+  div.className = 'student-search';
+  const input = document.createElement('input');
+  input.placeholder = 'Search students here';
+  div.appendChild(input);
+  const button = document.createElement('button');
+  button.textContent = 'Search';
+  div.appendChild(button);
+  divHeader.appendChild(div);
+}
+
 //create a function to append links onto page.
-function appendPageLinks(){
+const appendPageLinks = (list, activeLink) => {
   //select div so that we can append to it
   const div = document.querySelector('.page');
+  //if links are on page remove them
+  if(div.lastElementChild.className === 'pagination'){
+    const removeOldLinks = div.lastElementChild;
+    div.removeChild(removeOldLinks);
+  }
   //create div which we will append into div on page
   const newDiv = document.createElement('div');
-  //adding className for styling
   newDiv.className = 'pagination';
-  //append newDiv into div on page
   div.appendChild(newDiv);
   //create ul which will append into div
   const ul = document.createElement('ul');
-  //apend into newdiv
   newDiv.appendChild(ul);
   //calculate the amount of links we will need on page. takes length of students on page, divides by 10 then rounds to the nearest integer. e.g. 54 is length/10= 5.4 applying Math.ceil gives us 6 buttons.
-  const numOfLinks = Math.ceil((studentsList.length / 10));
-  //function that will create a link with param of pagenumber
-  function createLink(pageNumber){
-    //create li elem
+  const numOfLinks = Math.ceil((list.length / 10));
+  //function that will create a link with param of of the link number and which link will have the class 'active'
+  const createLink = (pageNumber, active) => {
+    //create li and links elems and append to ul
     const li = document.createElement('li');
-    //append it into ul which we created in appendPageLinks func
     ul.appendChild(li);
-    //create our link element
     const link = document.createElement('a');
     //set textContent to the parameter passed into the func
     link.textContent = pageNumber;
-    //we want the page to start with page 1 showing, so we will give it the active class and all links get the pageLink class
-    if(link.textContent === '1'){
+    //we want the page to ith page 1 showing, so we will give it the active class and all links get the pageLink class
+    if(link.textContent == active){
       link.className = 'pageLink active';
     } else{
       link.className = 'pageLink';
@@ -66,21 +88,63 @@ function appendPageLinks(){
   //loop through our const which contains the amount of links we need to show
   for(let i = 1; i <= numOfLinks; i++){
       //call the create link func as many times as we need. i is passed as argument which will be converted as link number inside func
-      createLink(i);
+      createLink(i, activeLink);
   }
 }
 //declaring  function which we will call to set our page up when loaded
-function initPage(){
-  //creates our links
-  appendPageLinks();
-  //ensures that page 1 will be the default page when opened
-  showPage(1);
+const initPage = () => {
+  showPage(1, studentsList);
+  createInputElement();
+  appendPageLinks(studentsList,1);
+  createMessageBox();
+}
+
+//func that will create a messagebox that will be displayed if no results found
+const createMessageBox = () => {
+  const p = document.createElement('p');
+  p.textContent = 'Unfortunately there are no students that match your search. Please try again.';
+  p.className = 'message-box';
+  p.style.display = 'none';
+  const linksDiv = document.querySelector('.pagination');
+  div.insertBefore(p, linksDiv);
+}
+
+//func for search box, create new array, loop through students and if matches are found add them to new array, then pass new arr into our funcs to display results
+const search = () => {
+  const newLis = [];
+  const input = document.querySelector('input');
+  const nameToSearch = input.value;
+  const mBox = document.querySelector('.message-box');
+
+  if(nameToSearch === ''){
+    showPage(1, studentsList);
+    appendPageLinks(studentsList,1);
+    mBox.style.display = 'none';
+  } else{
+    for(let i = 0; i < studentsList.length; i++){
+      let studentName = studentsList[i].firstElementChild.querySelector('h3').innerHTML;
+      if(studentName === nameToSearch){
+        newLis.push(studentsList[i]);
+      }
+    }
+    //if array is empty show 0 results and display messagebox, else show results
+    if(newLis.length === 0){
+      mBox.style.display = '';
+      appendPageLinks(newLis,0);
+      showPage(1, newLis);
+    } else {
+      showPage(1, newLis);
+      appendPageLinks(newLis,1);
+      mBox.style.display = 'none';
+    }
+  }
 }
 //Called to init page
 initPage();
 
 //Event Listener for clicks on div
 div.addEventListener('click', (event) => {
+  const studentsList = document.getElementsByClassName('student-item');
   //declare const for what was clicked
   const linkClicked = event.target;
   //Only run code if the click event was on a link we created
@@ -96,6 +160,18 @@ div.addEventListener('click', (event) => {
     //grab textcontent of link that was clicked
     const pageNumber = linkClicked.textContent;
     //use parseint to convert pagenumber to integer, then pass as argument to showpage func
-    showPage(parseInt(pageNumber));
+    showPage(parseInt(pageNumber), studentsList);
+  }
+});
+
+//Event listeners for search box, 1 for keyup 1 for click on search button
+divHeader.addEventListener('keyup', (event) => {
+  const input = event.target;
+  search();
+});
+divHeader.addEventListener('click', (event) => {
+  const button = event.target;
+  if(button.textContent === 'Search'){
+    search();
   }
 });
